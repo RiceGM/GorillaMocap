4ed21b4d0fec4c01dfccfa60508f1ea5fccbdb22
RiceGM <off.ricegm@gmail.com>
2023-07-06 13:53:46 +0300

    Add project files.

---
+++ GorillaMocap/MocapPatcher.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorillaMocap
+{
+    [BepInPlugin("org.ricegm.gorillamocap", "GorillaMocap", "1.0.0")]
+    public class MocapPatcher : BaseUnityPlugin 
+    {
+    }
+}
